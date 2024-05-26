ab831de0a65bf6ecf411af923f07f77161cf9a9b
Anand R P <anand.rp2030@gmail.com>
2024-05-26 21:04:00 +0530

    redux user  configuration completed

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,1 +1,12 @@
-// todo=> create store here
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import userReducer from './reducers/userSlilce'
+const rootReducer = combineReducers({
+    user: userReducer
+    // todo=>other reducers here
+})
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const store = configureStore({
+    reducer: rootReducer
+})
