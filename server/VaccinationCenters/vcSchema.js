6242e9e10f150f58330865a4e261001a5363a04b
soumyasics <soumya.sics@gmail.com>
2024-06-07 17:50:23 +0530

    Merge branch 'main' of https://github.com/soumyasics/smart-parent-chmm into t4

---
+++ server/VaccinationCenters/vcSchema.js
@@ -0,0 +1,55 @@
+
+const mongoose = require("mongoose");
+
+const vcSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    cateogry: {
+        enum: ["anganvadi","hospital"],
+
+      type: String,
+      required: true,
+    },
+    isActive: {
+  
+      type: Boolean,
+      default:false,
+    },
+  
+    profilePicture: {
+      type: Object,
+      default: null
+    }
+  },
+  { timestamps: true }
+);
+const  vcModel = mongoose.model("vaccinationcenters", vcSchema);
+module.exports = { vcModel };
+
+
+
+
+
+
+
+
