bd6db1aac8071da46a6c50405a12da50a1c96d89
Anand R P <anand.rp2030@gmail.com>
2024-06-18 21:53:30 +0530

    chat messages time added

--- client/src/pages/parent/chatWithVC/chatSection/Message.tsx
+++ client/src/pages/parent/chatWithVC/chatSection/Message.tsx
@@ -1,3 +1,4 @@
+import { formatTimeToIST } from "../../../../utils/time/formatTimeToIst.ts";
 import { ChatMessage } from "../types.ts";
 import { FC } from "react";
 interface MessageStartProps {
@@ -14,43 +15,43 @@ export const MessageStart: FC<MessageStartProps> = ({
   profilePic,
   message,
 }) => {
-  console.log("message", message);
+  const createdAt = message?.createdAt || null;
+  const timeInIST = createdAt ? formatTimeToIST(createdAt) : "";
+
   return (
     <div>
-      <div className="tw-chat tw-chat-start">
+      <div className="tw-chat tw-chat-start tw-mt-5">
         <div className="tw-chat-image tw-avatar">
           <div className="tw-w-10 tw-rounded-full">
-            <img
-              alt="Tailwind CSS chat bubble component"
-              src={profilePic}
-            />
+            <img alt="Tailwind CSS chat bubble component" src={profilePic} />
           </div>
         </div>
-        <div className="tw-chat-header tw-text-gray-300 tw-text-xs tw-opacity-50">
-          Obi-Wan Kenobi
-          <time className="tw-text-xs tw-opacity-50 tw-ms-3">12:45</time>
-        </div>
         <div className="tw-chat-bubble">{message?.message || ""}</div>
-        <div className="tw-chat-footer tw-opacity-50">Delivered</div>
+        <div className="tw-chat-footer tw-text-xs tw-text-gray-300 tw-opacity-50 ">
+          {timeInIST}
+        </div>
       </div>
     </div>
   );
 };
 
-export const MessageEnd: FC<MessageEndProps> = ({ message, profilePic }) => {
+export const MessageEnd: FC<MessageEndProps> = ({ profilePic, message }) => {
+  const createdAt = message?.createdAt || null;
+  const timeInIST = createdAt ? formatTimeToIST(createdAt) : "";
+
   return (
-    <div className={`tw-chat tw-chat-end`}>
+    <div className={`tw-chat tw-chat-end tw-mt-5`}>
       <div className="tw-chat-image tw-avatar">
         <div className="tw-w-10 tw-rounded-full">
           <img alt="Tailwind CSS chat bubble component" src={profilePic} />
         </div>
       </div>
-      <div className="tw-chat-header">
-        Obi-Wan Kenobi
-        <time className="tw-text-xs tw-opacity-50">12:45</time>
+      <div className="tw-chat-header tw-text-gray-300 tw-text-xs tw-opacity-50">
       </div>
       <div className="tw-chat-bubble">{message?.message || ""}</div>
-      <div className="tw-chat-footer tw-opacity-50">Sent</div>
+      <time className="tw-text-xs tw-text-gray-300 tw-opacity-50">
+        {timeInIST}
+      </time>
     </div>
   );
 };
